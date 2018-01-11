8c34b54e5527d1f4f5e91a344b9c525e44bcaaa3
Hunter <edhunter83@gmail.com>
2018-01-10 23:52:36 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@
 // Dependencies
 var Sequelize = require("sequelize");
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("pathfinderDB", "root", "root", {
-  host: "localhost",
+var sequelize = new Sequelize("yj0c1ekp5mdz7rru", "awxndxezk8crbqik", "oq6g369xnava4u4n", {
+  host: "h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
   pool: {
     max: 5,
